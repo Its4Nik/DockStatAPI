6b24a25d1952c6a18507ef27768e6e06919bcd19
ItsNik <info@itsnik.de>
2025-01-10 20:30:59 +0100

    Fix: Adjusted routes since they need an absolute path

--- src/routes/graphs/routes.ts
+++ src/routes/graphs/routes.ts
@@ -1,15 +1,27 @@
 import { Request, Response, Router } from "express";
 import { createResponseHandler } from "../../handlers/response";
-import { getGraphFilePaths } from "../../handlers/graph";
 import path from "path";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const ResponseHandler = createResponseHandler(res);
   try {
-    const graphPaths = getGraphFilePaths();
-    const graphPath = path.join(__dirname, "/../../../" + graphPaths.html);
+    const graphPath = path.join(
+      __dirname,
+      "/../../.." + "/src/data/graph.html",
+    );
     return res.contentType("html").status(200).sendFile(graphPath);
+  } catch (error: unknown) {
+    const errorMsg = error instanceof Error ? error.message : String(error);
+    return ResponseHandler.critical(errorMsg);
+  }
+});
+
+router.get("/image", async (req: Request, res: Response) => {
+  const ResponseHandler = createResponseHandler(res);
+  try {
+    const graphPath = path.join(__dirname, "/../../.." + "/src/data/graph.png");
+    return res.contentType("image/png").status(200).sendFile(graphPath);
   } catch (error: unknown) {
     const errorMsg = error instanceof Error ? error.message : String(error);
     return ResponseHandler.critical(errorMsg);

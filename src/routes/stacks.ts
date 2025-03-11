35f70f831e3733e09662143292bd118d9f217f69
ItsNik <info@itsnik.de>
2025-03-11 22:22:44 +0100

    Avoid unneeded ternary statements (simplify-ternary)
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/routes/stacks.ts
+++ src/routes/stacks.ts
@@ -16,9 +16,8 @@ export const stackRoutes = new Elysia({ prefix: "/stacks" })
         "/deploy",
         async ({ set, body }) => {
             try {
-                const isCustom = body.isCustom
-                    ? body.isCustom
-                    : false;
+                const isCustom = body.isCustom || false;
+
 
                 const image_updates = body.image_updates || false;
 
